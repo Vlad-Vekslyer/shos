05edc5d8a403a616d10cb35ae5b25c4fa26376b3
Vlad-Vekslyer <vladi00345@gmail.com>
2022-07-17 23:02:39 -0700

    it works!!!!!!!!!!

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 run-web:
 	cd ./react; npm run start
+
+compile-wasm:
+	cd ./rust; wasm-pack build --out-dir ../react/src/pkg --target web
--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,14 @@
 import System from "./components/System";
+import init, { greet } from "./pkg/rust";
 
 function App() {
+  init().then(() => {
+    greet();
+  }).catch(e => {
+    console.error(e);
+  })
+
+
   return (
     <>
       <System />
