c1735eb57a93711f6c0c5317dc4221c72d142626
Vlad-Vekslyer <vladi00345@gmail.com>
2022-07-19 21:56:12 -0700

    add some basic styling

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,6 +3,7 @@ import { When } from "react-if";
 import { useAsyncEffect } from "use-async-effect";
 import System from "./components/System";
 import initWasm from "./pkg/rust";
+import GlobalStyle from "./styles/global";
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -13,9 +14,12 @@ function App() {
   }, [])
 
   return (
-    <When condition={!loading}>
-      <System />
-    </When>
+    <>
+      <GlobalStyle />
+      <When condition={!loading}>
+        <System />
+      </When>
+    </>
   )
 }
 
