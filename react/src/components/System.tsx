cbfa60a2b97a85f380a4119c5a313e7de6da1777
Vlad-Vekslyer <vladi00345@gmail.com>
2022-07-23 13:24:34 -0700

    camera perspective

--- react/src/components/System.tsx
+++ react/src/components/System.tsx
@@ -7,13 +7,19 @@ export default function System(): JSX.Element {
 	useEffect(() => {
 		const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
 		camera.position.z = 1;
+		camera.position.y = 0.5;
+		camera.position.x = -0.6;
+		camera.rotateX(-0.25);
+		camera.rotateY(-0.3)
 
 		const scene = new THREE.Scene();
 
-		const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+		const axesHelper = new THREE.AxesHelper(1);
+		const geometry = new THREE.SphereGeometry(0.2);
 		const material = new THREE.MeshNormalMaterial();
 
 		const mesh = new THREE.Mesh(geometry, material);
+		scene.add(axesHelper);
 		scene.add(mesh);
 
 		const renderer = new THREE.WebGLRenderer({ antialias: true });
