909bfc8ddd5372fa7a3a3676f95551058a0bdd09
Vlad-Vekslyer <vladi00345@gmail.com>
2022-08-02 19:21:22 -0700

    working on wasm issue

--- react/src/components/AnimationLoop.tsx
+++ react/src/components/AnimationLoop.tsx
@@ -3,43 +3,37 @@ import { useEffect } from "react";
 import * as THREE from "three";
 import getSphereName from "../3d-utils/getSphereName";
 import isometricCamera from "../3d-utils/isometricCamera";
-import init, { System } from "../pkg/rust";
+import { InitOutput, System } from "../pkg/rust";
 
 interface IAnimationLoop {
   scene: THREE.Scene,
   renderer: THREE.WebGL1Renderer,
-  system: System
+  system: System,
+  wasm: InitOutput
 }
 
-export default function AnimationLoop({ scene, renderer, system }: IAnimationLoop): JSX.Element {
+export default function AnimationLoop({ scene, renderer, system, wasm }: IAnimationLoop): JSX.Element {
   const animationRequestId = useRef<number>();
 
   function tick() {
-    init().then(wasm => {
-      const stream = system.tick();
-      const planetCoordinates = new Float32Array(wasm.memory.buffer, stream.offset(), stream.size());
+    const stream = system.tick();
+    const planetCoordinates = new Float32Array(wasm.memory.buffer, stream.offset(), stream.size());
+    console.log("planetCoordinates", planetCoordinates);
 
-      for (let i = 0; i < planetCoordinates.length; i += 3) {
-        const sphere = scene.getObjectByName(getSphereName(i / 3));
-        console.log("Sphere", sphere);
-        if (!sphere) throw new Error("Missing sphere in scene");
+    for (let i = 0; i < planetCoordinates.length; i += 3) {
+      const sphere = scene.getObjectByName(getSphereName(i / 3));
+      if (!sphere) throw new Error("Missing sphere in scene");
 
-        sphere.position.x = planetCoordinates[i];
-        sphere.position.z = planetCoordinates[i + 1];
-      }
-      renderer.render(scene, isometricCamera);
+      sphere.position.x = planetCoordinates[i];
+      sphere.position.z = planetCoordinates[i + 1];
+    }
+    renderer.render(scene, isometricCamera);
 
-      animationRequestId.current = requestAnimationFrame(tick);
-    })
+    animationRequestId.current = requestAnimationFrame(tick);
   }
 
   useEffect(() => {
     tick();
-    return () => {
-      if (animationRequestId.current) {
-        cancelAnimationFrame(animationRequestId.current)
-      }
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
--- react/src/components/System.tsx
+++ react/src/components/System.tsx
@@ -1,11 +1,10 @@
-import { useRef, useState } from "react";
-import { When } from "react-if";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { useAsyncEffect } from "use-async-effect";
 import getSphereName from "../3d-utils/getSphereName";
 
 import isometricCamera from "../3d-utils/isometricCamera";
-import init, { System as WasmSystem } from "../pkg/rust";
+import init, { InitOutput, System as WasmSystem } from "../pkg/rust";
 import AnimationLoop from "./AnimationLoop";
 
 interface Planet {
@@ -15,56 +14,67 @@ interface Planet {
 }
 
 export default function System(): JSX.Element {
-	const systemRef = useRef<HTMLDivElement>(null)
+	const systemContainer = useRef<HTMLDivElement>(null)
 	const scene = useRef(new THREE.Scene());
 	const renderer = useRef(new THREE.WebGL1Renderer({ antialias: true }));
 	const system = useRef<WasmSystem>();
+	const wasm = useRef<InitOutput>();
 	const [loading, setLoading] = useState(true);
 
-	useAsyncEffect(async () => {
-		if (!systemRef.current) throw new Error("Missing system container")
-		systemRef.current.appendChild(renderer.current.domElement);
-
-		const wasm = await init();
-		system.current = new WasmSystem();
-		const stream = system.current.getPlanetCoordinates();
-		const planetCoordinates = new Float32Array(wasm.memory.buffer, stream.offset(), stream.size());
-
-		if (planetCoordinates.length % 3 !== 0) throw new Error("Wasm memory buffer length must be divisible by 3")
-
-		const material = new THREE.MeshNormalMaterial();
-
-		for (let i = 0; i < planetCoordinates.length; i += 3) {
-			const planet: Planet = {
-				x: planetCoordinates[i],
-				y: planetCoordinates[i + 1],
-				radius: planetCoordinates[i + 2]
-			}
-			const sphere = new THREE.SphereGeometry(planet.radius);
-			const mesh = new THREE.Mesh(sphere, material);
-			mesh.position.x = planet.x;
-			mesh.position.z = planet.y;
-			mesh.name = getSphereName(i / 3);
-			scene.current.add(mesh);
-		}
+	useEffect(() => {
+		if (!systemContainer.current) throw new Error("Missing system container")
 
 		const axesHelper = new THREE.AxesHelper(1);
 		scene.current.add(axesHelper);
 
 		renderer.current.setSize(window.innerWidth, window.innerHeight);
 		renderer.current.render(scene.current, isometricCamera);
-		setLoading(false);
-	}, () => {
-		if (!systemRef.current) throw new Error("Missing system container")
-		systemRef.current.removeChild(renderer.current.domElement)
-	}, []);
+
+		systemContainer.current.appendChild(renderer.current.domElement);
+
+		return () => {
+			if (!systemContainer.current) throw new Error("Missing system container")
+			systemContainer.current.removeChild(renderer.current.domElement);
+			renderer.current.clear();
+			scene.current.clear();
+		}
+	}, [])
+
+	// useAsyncEffect(async () => {
+	// 	const wasmObj = await init();
+	// 	const systemObj = new WasmSystem();
+	// 	const stream = systemObj.getPlanetCoordinates();
+	// 	const planetCoordinates = new Float32Array(wasmObj.memory.buffer, stream.offset(), stream.size());
+
+	// 	if (planetCoordinates.length % 3 !== 0) throw new Error("Wasm memory buffer length must be divisible by 3")
+
+	// 	const material = new THREE.MeshNormalMaterial();
+
+	// 	for (let i = 0; i < planetCoordinates.length; i += 3) {
+	// 		const planet: Planet = {
+	// 			x: planetCoordinates[i],
+	// 			y: planetCoordinates[i + 1],
+	// 			radius: planetCoordinates[i + 2]
+	// 		}
+	// 		const sphere = new THREE.SphereGeometry(planet.radius);
+	// 		const mesh = new THREE.Mesh(sphere, material);
+	// 		mesh.position.x = planet.x;
+	// 		mesh.position.z = planet.y;
+	// 		mesh.name = getSphereName(i / 3);
+	// 		scene.current.add(mesh);
+	// 	}
+
+	// 	wasm.current = wasmObj;
+	// 	system.current = systemObj;
+	// 	setLoading(false);
+	// }, []);
 
 	return (
 		<>
-			<div className="system" ref={systemRef} />
-			<When condition={!loading}>
-				<AnimationLoop scene={scene.current} renderer={renderer.current} system={system.current as WasmSystem} />
-			</When>
+			<div className="system" ref={systemContainer} />
+			{!loading && system.current && wasm.current ?
+				<AnimationLoop scene={scene.current} wasm={wasm.current} renderer={renderer.current} system={system.current} />
+				: null}
 		</>
 	)
 }
