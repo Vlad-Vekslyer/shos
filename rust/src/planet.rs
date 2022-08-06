eb8cbe4b5958735a2941fa4a52ae7ecaecc17195
Vlad-Vekslyer <vladi00345@gmail.com>
2022-08-05 22:07:04 -0700

    eccentricity calculation

--- rust/src/planet.rs
+++ rust/src/planet.rs
@@ -1,9 +1,30 @@
+fn calculate_eccentricity(initial_x: f32, initial_y: f32, semi_major_axis: f32) -> f32 {
+    let is_at_angle = initial_y != 0.0;
+    let aphelion = match is_at_angle {
+        false => initial_x.abs(),
+        true => calculate_pythagorean(initial_y.abs(), initial_x.abs()),
+    };
+    let perihelion = (semi_major_axis * 2.0) - aphelion;
+
+    (aphelion - perihelion) / (aphelion + perihelion)
+}
+
+fn calculate_pythagorean(a: f32, b: f32) -> f32 {
+    (a.powf(2.0) + b.powf(2.0)).sqrt()
+}
+
 pub struct Planet {
     pub radius: f32,
+    eccentricity: f32,
+    semi_major_axis: f32,
 }
 
 impl Planet {
-    pub fn new(radius: f32) -> Planet {
-        Planet { radius }
+    pub fn new(radius: f32, initial_x: f32, initial_y: f32, semi_major_axis: f32) -> Planet {
+        Planet {
+            radius,
+            semi_major_axis,
+            eccentricity: calculate_eccentricity(initial_x, initial_y, semi_major_axis),
+        }
     }
 }
