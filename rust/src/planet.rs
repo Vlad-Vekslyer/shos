cc75efbb4c171c48d7e28313f89041e7505cbbe0
Vlad-Vekslyer <vladi00345@gmail.com>
2022-08-06 20:05:00 -0700

    debugging the math

--- rust/src/planet.rs
+++ rust/src/planet.rs
@@ -1,5 +1,7 @@
 use web_sys;
 
+use crate::utils::set_panic_hook;
+
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
@@ -12,16 +14,24 @@ fn calculate_angle(initial_x: f32, initial_y: f32) -> f32 {
     let side_c = initial_y.abs();
     let side_b = calculate_pythagorean(side_a, side_c);
 
-    (side_b.powi(2) + side_c.powi(2) - side_a.powi(2)) / (2.0 * side_b * side_c).acos()
+    log!(
+        "angle {}(radians)",
+        ((side_b.powi(2) + side_c.powi(2) - side_a.powi(2)) / (2.0 * side_b * side_c)).acos()
+    );
+    ((side_b.powi(2) + side_c.powi(2) - side_a.powi(2)) / (2.0 * side_b * side_c)).acos()
+}
+
+fn calculate_perihelion(initial_x: f32, initial_y: f32) -> f32 {
+    let is_at_angle = initial_y != 0.0;
+    match is_at_angle {
+        false => initial_x.abs(),
+        true => calculate_pythagorean(initial_y.abs(), initial_x.abs()),
+    }
 }
 
 // https://sciencing.com/calculate-period-orbit-5840979.html
 fn calculate_eccentricity(initial_x: f32, initial_y: f32, semi_major_axis: f32) -> f32 {
-    let is_at_angle = initial_y != 0.0;
-    let perihelion = match is_at_angle {
-        false => initial_x.abs(),
-        true => calculate_pythagorean(initial_y.abs(), initial_x.abs()),
-    };
+    let perihelion = calculate_perihelion(initial_x, initial_y);
     let aphelion = (semi_major_axis * 2.0) - perihelion;
     log!("aphelion {}", aphelion);
     log!("perihelion {}", perihelion);
@@ -56,6 +66,9 @@ pub struct Planet {
 
 impl Planet {
     pub fn new(radius: f32, initial_x: f32, initial_y: f32, semi_major_axis: f32) -> Planet {
+        set_panic_hook();
+        Planet::validate(initial_x, initial_y, semi_major_axis);
+
         let eccentricity = calculate_eccentricity(initial_x, initial_y, semi_major_axis);
         let semi_minor_axis = calculate_semi_minor_axis(semi_major_axis, eccentricity);
         let angle = calculate_angle(initial_x, initial_y);
@@ -68,6 +81,14 @@ impl Planet {
             angle,
         }
     }
+
+    fn validate(initial_x: f32, initial_y: f32, semi_major_axis: f32) {
+        let perihelion = calculate_perihelion(initial_x, initial_y);
+        if perihelion >= semi_major_axis {
+            panic!("Perihelion cannot be larger than the semi major axis");
+        }
+    }
+
     // https://math.stackexchange.com/questions/426150/what-is-the-general-equation-of-the-ellipse-that-is-not-in-the-origin-and-rotate
     pub fn calculate_next_y_coord(&self, next_x: f32, current_y: f32) -> f32 {
         let next_y = (self.semi_minor_axis * (self.semi_major_axis.powi(2) - next_x.powi(2)))
