b75d3a9d8e45f92ed3d90dc7a54810516de1754d
Vlad-Vekslyer <vladi00345@gmail.com>
2022-08-20 14:10:22 -0700

    load textures

--- react/src/three-utils/scene.ts
+++ react/src/three-utils/scene.ts
@@ -11,25 +11,43 @@ import makemakeTexture from '../assets/makemake.jpg';
 import marsTexture from '../assets/mars.jpg';
 import saturnTexture from '../assets/saturn.jpg';
 
-export async function addPlanetsToScene(planets: Planet[], scene: THREE.Scene): Promise<void> {
-  const promises = planets.map((planet, i) => {
-    return new Promise<void>(resolve => {
-      new THREE.TextureLoader().load(mercuryTexture, texture => {
-        const material = new THREE.MeshStandardMaterial({ map: texture });
-        const sphere = new THREE.SphereGeometry(planet.radius);
-        const mesh = new THREE.Mesh(sphere, material);
-
-        mesh.position.x = planet.x;
-        mesh.position.z = planet.y;
-        mesh.name = getSphereName(i);
-        mesh.receiveShadow = true;
-
-        scene.add(mesh);
-        resolve();
-      });
+function loadTextures(): Promise<THREE.Texture[]> {
+  const textureLoader = new THREE.TextureLoader();
+  const promises = [
+    mercuryTexture,
+    ceresTexture,
+    earthTexture,
+    erisTexture,
+    haumeaTexture,
+    makemakeTexture,
+    marsTexture,
+    saturnTexture
+  ].map(texture => {
+    return new Promise<THREE.Texture>(resolve => {
+      textureLoader.load(texture, loadedTexture => resolve(loadedTexture))
     })
+  })
+
+  return Promise.all(promises);
+}
+
+export async function addPlanetsToScene(planets: Planet[], scene: THREE.Scene): Promise<void> {
+  const textures = await loadTextures();
+  planets.forEach((planet, i) => {
+    const randomTextureIndex = Math.floor(Math.random() * textures.length); // from 0 to textures.length - 1
+    const texture = textures[randomTextureIndex];
+
+    const material = new THREE.MeshStandardMaterial({ map: texture });
+    const sphere = new THREE.SphereGeometry(planet.radius);
+    const mesh = new THREE.Mesh(sphere, material);
+
+    mesh.position.x = planet.x;
+    mesh.position.z = planet.y;
+    mesh.name = getSphereName(i);
+    mesh.receiveShadow = true;
+
+    scene.add(mesh);
   });
-  await Promise.all(promises);
 }
 
 export function addHelpersToScene(scene: THREE.Scene, light: THREE.PointLight): void {
